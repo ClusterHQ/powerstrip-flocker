0cbcde9810587933be21362c9b581da90170f695
Luke Marsden <luke@clusterhq.com>
2015-02-25 15:15:12 +0000

    sample yaml settings

--- vagrant-aws/utils.py
+++ vagrant-aws/utils.py
@@ -1,8 +1,12 @@
 from pipes import quote
 import subprocess
+import json
+
+config = json.load(open("settings.yml"))
 
 def runSSH(ip, command):
-    command = 'ssh ubuntu@%s %s' % (ip, " ".join(map(quote, command)))
+    command = 'ssh -i %s ubuntu@%s %s' % (config["private_key_path"],
+            ip, " ".join(map(quote, command)))
     return subprocess.check_output(command, shell=True)
 
 def pushConfig(text, instances):
@@ -13,7 +17,8 @@ def pushConfig(text, instances):
     print "Written master address"
     for (externalIP, internalIP) in instances:
         runSSH(externalIP, ['sudo', 'mkdir', '-p', '/etc/flocker'])
-        scp = "scp master_address hybrid@%s:/tmp/master_address" % (externalIP,)
+        scp = "scp -i %s master_address hybrid@%s:/tmp/master_address" % (
+                config["private_key_path"], externalIP,)
         subprocess.check_output(scp, shell=True)
         runSSH(externalIP, ['sudo', 'mv', '/tmp/master_address', '/etc/flocker/master_address'])
         print "Pushed master address to %s" % (externalIP,)
