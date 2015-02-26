f9c428598183984d37fe1542f79e4ea5e4cccc91
Luke Marsden <luke@clusterhq.com>
2015-02-26 11:34:38 +0000

    lists

--- vagrant-aws/push-config.py
+++ vagrant-aws/push-config.py
@@ -6,7 +6,7 @@ import subprocess
 print "Gathering internal & external IP addresses from Vagrant..."
 
 instances = []
-for n in range(1, 3):
+for n in range(1, 3): # 2 hosts
     command = "vagrant ssh-config node%d 2>/dev/null |grep HostName" % (n,)
     result = subprocess.check_output(command, shell=True)
     externalIP = result.split()[1]
@@ -22,5 +22,5 @@ utils.pushConfig(masterExternal, instances)
 
 # install powerstrip, powerstrip-flocker, and configure both nodes to start
 # flocker-control and flocker-zfs service.
-utils.runSSHPassthru(masterExternal, "/vagrant/install.sh master")
-utils.runSSHPassthru(minionExternal, "/vagrant/install.sh minion")
+utils.runSSHPassthru(masterExternal, ["/vagrant/install.sh", "master"])
+utils.runSSHPassthru(minionExternal, ["/vagrant/install.sh", "minion"])
