385e0e7f0236de045b8259fbe8640f6e78b8a12e
Luke Marsden <luke@clusterhq.com>
2015-05-07 19:43:31 +0100

    another twiddle

--- powerstripflocker/test/test_acceptance.py
+++ powerstripflocker/test/test_acceptance.py
@@ -89,7 +89,8 @@ class PowerstripFlockerTests(TestCase):
                    "docker run --privileged --rm "
                        "-e DOCKER_GITCOMMIT=`git log -1 --format=%%h` "
                        "-v %(dockerDir)s:/go/src/github.com/docker/docker "
-                       "custom-docker hack/make.sh binary" % dict(dockerDir=BUILDSLAVE_DOCKER_DIR))
+                       "custom-docker hack/make.sh binary" % dict(
+                           dockerDir=BUILDSLAVE_DOCKER_DIR))
             print "Running docker command:", dockerCmd
             exit = system(dockerCmd)
             if exit > 0:
