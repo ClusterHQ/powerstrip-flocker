b9cc736411fe1582ae6c6f2058bf2ef6f393004b
Luke Marsden <luke@clusterhq.com>
2015-05-12 14:15:56 +0100

    fix bug

--- powerstripflocker/test/test_acceptance.py
+++ powerstripflocker/test/test_acceptance.py
@@ -244,7 +244,7 @@ class PowerstripFlockerTests(TestCase):
                        "git clone https://github.com/clusterhq/powerstrip-flocker && "
                        "cd powerstrip-flocker && "
                        "git checkout %s && " % (PF_VERSION,)
-                       + "FLOCKER_CONTROL_SERVICE_BASE_URL=%s" % (BASE_PATH, self.cluster.base_url,)
+                       + "FLOCKER_CONTROL_SERVICE_BASE_URL=%s" % (self.cluster.base_url,)
                        + " MY_NETWORK_IDENTITY=%s" % (ip,)
                        + " MY_HOST_UUID=%s" % (host_uuid,)
                        + " twistd -noy powerstripflocker.tac")
