2fb056e59206098b1f6d28d237574baffdb2ebc8
Luke Marsden <luke@clusterhq.com>
2015-05-14 16:49:42 +0100

    twiddles

--- build-docker-and-push.sh
+++ build-docker-and-push.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # run this from inside the docker dir
-docker run --privileged --rm -e DOCKER_GITCOMMIT=`git log -1 --format=%%h` -v `pwd`:/go/src/github.com/docker/docker fix_unsupported_protocol_scheme hack/make.sh binary
+docker run --privileged --rm -e DOCKER_GITCOMMIT=`git log -1 --format=%%h` -v `pwd`:/go/src/github.com/docker/docker custom-docker hack/make.sh binary
 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@$1 initctl stop docker
 scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ~/.ssh/id_rsa_flocker bundles/1.7.0-dev/binary/docker-1.7.0-dev root@$1:/usr/bin/docker
 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@$1 initctl start docker
