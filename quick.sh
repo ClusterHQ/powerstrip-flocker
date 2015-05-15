cca2f4ff68bb46c78e1b3640b0fa5621b17a1aaa
Luke Marsden <luke@clusterhq.com>
2015-05-15 16:09:41 +0100

    cherry-pick slimming of quick.sh

--- quick.sh
+++ quick.sh
@@ -18,35 +18,8 @@ set -x -e
 # This should match up with DOCKER_PULL_REPO in powerstripflocker/test/test_acceptance.py
 DOCKER_PULL_REPO="lmarsden"
 
-# This is a docker binary which gets scp'd onto the nodes...
-
-# Recipe to create this binary on the host:
-# $ export BRANCHNAME=some_docker_branch
-# $ mkdir -p ~/Projects; cd ~/Projects
-# $ if [ ! -e docker ]; then git clone git@github.com:clusterhq/docker-plugins; mv docker-plugins docker; fi
-# $ cd docker
-# $ git checkout $BRANCHNAME
-# $ docker build -t $BRANCHNAME .
-# $ docker run --privileged --rm -ti -v `pwd`:/go/src/github.com/docker/docker $BRANCHNAME hack/make.sh binary
-
-DOCKER_BINARY="${HOME}/Projects/docker/bundles/1.7.0-dev/binary/docker-1.7.0-dev"
-
-KEY="${HOME}/.ssh/id_rsa_flocker"
-DOCKER_PATH_ON_HOST="/usr/bin/docker"
-USER="root"
-
 NODE1="172.16.255.240"
 NODE2="172.16.255.241"
-
-for NODE in $NODE1 $NODE2; do
-    if [ -e $DOCKER_BINARY ]; then
-        echo "Uploading modified docker to test node..."
-        ssh -i $KEY $USER@$NODE initctl stop docker
-        scp -i $KEY $DOCKER_BINARY $USER@$NODE:$DOCKER_PATH_ON_HOST
-        ssh -i $KEY $USER@$NODE initctl start docker
-        ssh -i $KEY $USER@$NODE docker --version
-    fi
-done
 
 if [ "$1" != "--no-build" ]; then
     docker build -t ${DOCKER_PULL_REPO}/flocker-plugin:volume-plugin .
